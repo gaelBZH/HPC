68ff0c22d9564ddddd190af163e0a762e88df439
gaelBZH <gaelkerninon3110@gmail.com>
2026-04-16 20:44:44 +0200

    Laboratory 5

--- Laboratory5/TP5.c
+++ Laboratory5/TP5.c
@@ -1,207 +1,204 @@
-#include <stdlib.h>
+#include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
-#include <err.h>
 
-// Method : Compressed Row Storage (CRS)
+int sizes[] = {500, 1000, 2000, 3000, 4000};
+#define NUM_TESTS 5
+#define SPARSITY 0.1
 
-const int a = 10; // rows
-const int b = a; // cols
-
-int randint(int start, int end)
+void run_test(int N, int rank, int size)
 {
-  int l = rand() % (end-start) + start;
-  return l;
-}
+    // CRS Matrix
+    double *val = NULL;
+    int *col_ind = NULL;
+    int *row_ptr = NULL;
+    
+    // Calculate Remainder
+    int rows_per_proc = N / size;
+    int remainder = N % size;
+    int local_rows = (rank < remainder) ? rows_per_proc + 1 : rows_per_proc;
+    int start_row = (rank < remainder) ? rank * (rows_per_proc + 1) : rank * rows_per_proc + remainder;
 
-double randfloat()
-{
-  double c = (double)rand();
-  return c;
-}
-
-int main()
-{
-  srand(time(NULL));
-  
-  // Number of numbers different than 0 per row.
-  int k = b / 5; // 20% of the line. i.e. 2 on 10 columns.
-  if (k > b)
-    return 1; // k can't be grater than the number of columns.
+    double *full_vector = malloc(N * sizeof(double));
+    double *local_res = malloc(local_rows * sizeof(double));
+    double *final_res = NULL;
+    int total_notNullValues = 0;
+    double t1;
+    double t2;
 
 
-  // Creating the Matrix
-  double **M;
-  M = (double**)calloc(a, sizeof(double*));
-  for (int i = 0 ; i < a; i++)
-    M[i] = (double*)calloc(b, sizeof(double));
 
-  
-  // Filling the Matrix
-  for (int row = 0 ; row < a; row++)
-  {
-    // Generate k numbers on this line
-    int generated_numbers = 0;
-    while (generated_numbers < k)
+    // Create CRS Matrix
+    if (rank == 0)
     {
-        int col = randint(0, b);
-        if (M[row][col] == 0)
+        final_res = malloc(N * sizeof(double));
+        row_ptr = malloc((N + 1) * sizeof(int));
+        row_ptr[0] = 0;
+        val = malloc(N * N * SPARSITY * 2 * sizeof(double)); 
+        col_ind = malloc(N * N * SPARSITY * 2 * sizeof(int));
+
+        for (int i = 0; i < N; i++)
         {
-          M[row][col] = (float)rand()/RAND_MAX;
-          generated_numbers += 1;
+            int row_notNullValues = 0;
+            for (int j = 0; j < N; j++)
+            {
+                if ((double)rand() / RAND_MAX < SPARSITY)
+                {
+                    val[total_notNullValues] = (double)rand() / RAND_MAX;
+                    col_ind[total_notNullValues] = j;
+                    total_notNullValues += 1;
+                    row_notNullValues += 1;
+                }
+            }
+            row_ptr[i+1] = row_ptr[i] + row_notNullValues;
+            full_vector[i] = (double)rand() / RAND_MAX;
         }
-    } 
-  }
-
-  // Creating the Vector and Filling it
-  double *T = calloc(a, sizeof(double));
-  for (int i = 0; i < a; i++)
-    T[i] = (float)rand()/RAND_MAX;
-
-
-  // Printing maximum : 15x15
-  if (a <= 15 && b <= 15)
-  {
-    // Printing the Matrix
-    printf("--------- Original Matrix (%ix%i) : ---------\n", a, b);
-    for (int row = 0 ; row < a; row++)
-    {
-      for (int col = 0 ; col < b ; col++)
-      {
-        double value = M[row][col];
-        if (value == 0.0)
-          printf("   0    \t"); // Print '0' instead of '0.000000'
-        else
-          printf("%f\t", M[row][col]);
-      }
-      printf("\n");
+        t1 = MPI_Wtime();
     }
 
-    // Printing the Vector
-    printf("\n\n\n--------- Vector (%i) : ---------\n", a);
-    for (int i = 0; i < a; i++)
-      printf("%f\t", T[i]);
-    printf("\n");
-  }
 
 
-  // Record Created Matrix in CRS
-  double *val = calloc(a * k, sizeof(double));
-  int *col_ind = calloc(a * k, sizeof(int));
-  int *row_ptr = calloc(a + 1, sizeof(int));
+    // Broadcast
+    MPI_Bcast(full_vector, N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-  int i = 0;
-  for (int row = 0; row < a; row++)
-  {
-      row_ptr[row] = i; 
-      
-      for (int col = 0; col < b; col++)
-      {
-          double value = M[row][col];
-          if (value != 0)
-          {
-              val[i] = value;
-              col_ind[i] = col;
-              i++;
-          }
-      }
-  }
-  row_ptr[a] = i;
+    int notNullValues;
+    int *send_counts = malloc(size * sizeof(int));
+    int *displs = malloc(size * sizeof(int));
+    int *recv_counts = malloc(size * sizeof(int));
+    int *recv_displs = malloc(size * sizeof(int));
 
-  
-  // Print Vectors of the Recorded Matrix
-  printf("\n\n--------- CRS Matrix ---------\nval[] = ");
-  for (int i = 0; i < 2*a; i++)
-    printf("%f  ", val[i]);
-  printf("\n");
-  printf("col_ind[] = ");
-  for (int i = 0 ; i < 2*a; i++)
-    printf("%i  ", col_ind[i]);
-  printf("\n");
-  printf("row_ptr[] = ");
-  for (int i = 0; i < b; i++)
-    printf("%i  ", row_ptr[i]);
-  printf("\n\n\n");
+    if (rank == 0)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            int i_rows = (i < remainder) ? rows_per_proc + 1 : rows_per_proc;
+            int i_start = (i < remainder) ? i * (rows_per_proc + 1) : i * rows_per_proc + remainder;
+            
+            send_counts[i] = row_ptr[i_start + i_rows] - row_ptr[i_start];
+            displs[i] = row_ptr[i_start];
+            
+            recv_counts[i] = i_rows;
+            recv_displs[i] = i_start;
+        }
+    }
 
-  // Check the Equality
-  printf("--------- Checking the Equality beetween the two Matrix ---------\n");
-  int match = 1; // boolean
-  for (int r = 0; r < a; r++)
-  {
-      for (int c = 0; c < b; c++)
-      {
-          double dense_val = M[r][c];
-          double crs_val = 0;
+    MPI_Scatter(send_counts, 1, MPI_INT, &notNullValues, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-          // Search this column in the CRS
-          for (int i = row_ptr[r]; i < row_ptr[r+1]; i++)
-          {
-              if (col_ind[i] == c)
-              {
-                  crs_val = val[i];
-                  break;
-              }
-          }
+    double *local_val = malloc(notNullValues * sizeof(double));
+    int *local_col_ind = malloc(notNullValues * sizeof(int));
+    int *local_row_ptr = malloc((local_rows + 1) * sizeof(int));
 
-          if (dense_val != crs_val)
-          {
-              match = 0;
-              printf("Problem at (%i,%i). Original : %f, CRS : %f\n", r, c, dense_val, crs_val);
-          }
-      }
-  }
-  if (match)
-    printf("Verification Successful, the two Matrix are equal.\n\n\n");  
+    MPI_Scatterv(val, send_counts, displs, MPI_DOUBLE, local_val, notNullValues, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(col_ind, send_counts, displs, MPI_INT, local_col_ind, notNullValues, MPI_INT, 0, MPI_COMM_WORLD);
 
 
-  // for both standard and CRS multiplications,
-  // results are initialized with 0 values (calloc)
-  // so we don't need local "sum" variables, we only do
-  // vector += partial sum
-  
-  // Standard Multiplication
-  printf("--------- Standard Multiplication M x T ---------\n");
-  double* standard_result = calloc(a, sizeof(double));
-  for (int i = 0; i < a; i++)
-  {
-    for (int j = 0; j < b; j++)
-      standard_result[i] += M[i][j] * T[j];
-    printf("%f\t", standard_result[i]);
-  }
-  printf("\n\n\n");
 
-  // CRS Multiplication
-  printf("--------- CRS Multiplication M x T ---------\n");
-  double* crs_result = calloc(a, sizeof(double));
-  for (int i = 0; i < a; i++)
-  {
-    for (int k = row_ptr[i]; k < row_ptr[i+1]; k++)
-      crs_result[i] += val[k] * T[col_ind[k]];
-    printf("%f\t", standard_result[i]);
-  }
-  printf("\n\n\n");
 
-  // Procedure for Verification of the Results
-  printf("--------- Procedure for Verification of the Results ---------\n");
-  for (int i = 0; i < a; i++)
-    if (standard_result[i] != crs_result[i])
-      errx(EXIT_FAILURE, "Verification failed. Error on i=%i. Got standard_result[%i]=%f and crs_result[%i]=%f", i, i, standard_result[i], i, crs_result[i]);
-  printf("Verification Successfull, the two results of matrix-vector products are equal.");
-  printf("\n\n\n");
+    // Send the row_ptr parts
+    if (rank == 0)
+    {
+        for (int i = 1; i < size; i++)
+        {
+            int i_rows = recv_counts[i];
+            int i_start = recv_displs[i];
+            int offset = row_ptr[i_start];
+            int *temp_ptr = malloc((i_rows + 1) * sizeof(int));
+            for (int j = 0; j <= i_rows; j++) 
+                temp_ptr[j] = row_ptr[i_start + j] - offset;
+            MPI_Send(temp_ptr, i_rows + 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            free(temp_ptr);
+        }
+        for (int j = 0; j <= local_rows; j++)
+            local_row_ptr[j] = row_ptr[j];
+    }
+    else
+        MPI_Recv(local_row_ptr, local_rows + 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-  // Free Memory
-  printf("--------- Freeing Memory ---------\n");
-  for (int row = 0 ; row < a; row++)
-    free(M[row]);
-  free(M);
-  free(val);
-  free(col_ind);
-  free(row_ptr);
-  free(T);
-  free(standard_result);
-  free(crs_result);
-  printf("Memory has been freed.\n");
 
-  return 0;
+
+    // Calculate
+    for (int i = 0; i < local_rows; i++)
+    {
+        local_res[i] = 0.0;
+        for (int k = local_row_ptr[i]; k < local_row_ptr[i + 1]; k++)
+            local_res[i] += local_val[k] * full_vector[local_col_ind[k]];
+    }
+
+
+
+    // Get and Merge Results
+    MPI_Gatherv(local_res, local_rows, MPI_DOUBLE, final_res, recv_counts, recv_displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+
+
+    // Print Results
+    if (rank == 0)
+    {
+        t2 = MPI_Wtime();
+        printf("Matrix Size : %dx%d\t| Time : %f s\t", N, N, t2 - t1);
+
+        // Verification
+        int correct = 1; // boolean
+        for (int i = 0; i < N; i++)
+        {
+            double seq_val = 0;
+            for (int k = row_ptr[i]; k < row_ptr[i+1]; k++)
+                seq_val += val[k] * full_vector[col_ind[k]];
+            
+            if (abs(seq_val - final_res[i]) > 1e-7)
+                correct = 0;
+        }
+        if (correct)
+            printf("Verification Successfull\n");
+        else
+            printf("Verification Failed\n");
+
+        // Free Matrix
+        free(val);
+        free(col_ind);
+        free(row_ptr);
+        free(final_res);
+    }
+
+
+
+
+    // Free Memory
+    free(full_vector);
+    free(local_res);
+    free(local_val); 
+    free(local_col_ind);
+    free(local_row_ptr);
+    free(send_counts);
+    free(displs);
+    free(recv_counts);
+    free(recv_displs);
 }
 
+
+
+
+int main(int argc, char** argv)
+{
+    int rank; // process ID
+    int size; // number of processes
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0)
+    {
+        srand(time(NULL));
+        printf("=========== Test with %d processes ===========\n", size);
+    }
+
+    // Constant Number of Processes
+    for (int i = 0; i < NUM_TESTS; i++)
+        run_test(sizes[i], rank, size);
+
+    MPI_Finalize();
+    return 0;
+}
+
+// mpicc TP5.c -o emp && mpirun -np 8 ./emp
